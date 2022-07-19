5e141eef409341edc7291612ee61f8522d3a5600
Wenfeng Wang <kalot.wang@gmail.com>
2022-07-19 18:29:30 +0800

    update query endpoint

--- src/graph.default.js
+++ src/graph.default.js
@@ -1,4 +1,4 @@
 module.exports = {
     'kovan': 'https://api.thegraph.com/subgraphs/name/tolak/chainbridge-kovan',
-    'thala': 'https://api.subquery.network/sq/tolak/subbridge-subquery-test'
+    'thala': 'https://api.subquery.network/sq/tolak/subbridge-graphql-test0'
 }
