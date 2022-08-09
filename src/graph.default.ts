9cc825908deb0ecdda0d08683922e5caac3c3029
Wenfeng Wang <kalot.wang@gmail.com>
2022-08-09 17:06:28 +0800

    update endpoint of thala indexer

--- src/graph.default.ts
+++ src/graph.default.ts
@@ -1,4 +1,4 @@
 export const GraphEndpoint: {[key: string]: string} = {
   kovan: 'https://api.thegraph.com/subgraphs/name/tolak/chainbridge-kovan',
-  thala: 'https://api.subquery.network/sq/tolak/subbridge-graphql-test0',
+  thala: 'https://api.subquery.network/sq/Phala-Network/khala-subbridge-subquery__UGhhb',
 }
